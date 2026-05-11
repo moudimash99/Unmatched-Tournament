09258ce7640fc91303fde6febcaad0559b303e39
Mohammad Machaka <moudimash99@gmail.com>
2026-05-11 02:04:27 +0200

    organization

--- scripts/tournament_html.py
+++ scripts/tournament_html.py
@@ -444,6 +444,6 @@ renderAll();
 </body>
 </html>"""
 
-with open("tournament.html","w",encoding="utf-8") as f:
+with open("generated/tournament.html","w",encoding="utf-8") as f:
     f.write(HTML)
-print("\nHTML written to tournament.html")
+print("\nHTML written to generated/tournament.html")
